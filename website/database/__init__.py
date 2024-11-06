a84298293c5c408fef94c0e49734ab325d4879d2
Ayman Zaher <11262003a@gmail.com>
2024-11-06 17:30:46 -0500

    Added Flask-Migrate to database

--- website/database/__init__.py
+++ website/database/__init__.py
@@ -1,7 +1,9 @@
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate, stamp, upgrade
 from os import path
 
 db = SQLAlchemy()
+migrate = Migrate()
 
 def initialize_database(app, db_path):
     app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{db_path}'
@@ -10,8 +12,16 @@ def initialize_database(app, db_path):
     from . import user_model, meme_model
     db.init_app(app)
 
+    db_directory = path.dirname(__file__)
+
+    migrations_directory = path.join(db_directory, 'migrations')
+    migrate.init_app(app, db, directory=migrations_directory)
+
     with app.app_context():
         '''Initilizes the databse file and sets up the table.'''
         if not path.exists(db_path):
             db.create_all()
+            stamp()
             print('Created!')
+        else:
+            upgrade()
