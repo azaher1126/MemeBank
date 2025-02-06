be6275054cb29f8f0d58526bad13f880d5a328bb
Ayman Zaher <11262003a@gmail.com>
2025-02-06 17:39:02 -0500

    Improved run_tests script and removed unused code

--- run_tests.py
+++ run_tests.py
@@ -5,10 +5,13 @@ coverage_tracker = coverage.Coverage()
 coverage_tracker.start()
 
 test_suite = unittest.TestLoader().discover('tests')
-unittest.TextTestRunner(verbosity=2).run(test_suite)
+test_result = unittest.TextTestRunner(verbosity=2).run(test_suite)
 
 coverage_tracker.stop()
 coverage_tracker.save()
 
 print(coverage_tracker.report())
 coverage_tracker.html_report()
+
+if not test_result.wasSuccessful():
+    exit(1)
