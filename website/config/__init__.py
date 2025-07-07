eab1ba8d05c65f0dcd530c5abf6421afd7a5c1c4
Ayman Zaher <11262003a@gmail.com>
2025-07-07 14:50:35 -0400

    Fixed the config classes to allow use when a .env file is not present.

--- website/config/__init__.py
+++ website/config/__init__.py
@@ -2,30 +2,34 @@ from os import environ
 from secrets import token_hex
 
 class Config():
-    SECRET_KEY = environ.get("SECRET_KEY")
-    RECAPTCHA_PUBLIC_KEY = environ.get("RECAPTCHA_PUBLIC_KEY")
-    RECAPTCHA_PRIVATE_KEY = environ.get("RECAPTCHA_PRIVATE_KEY")
-    UPLOADS_BASE_PATH = environ.get("UPLOADS_BASE_PATH")
-    DB_PATH = environ.get('DB_PATH')
+    def __init__(self):
+        self.SECRET_KEY = environ.get("SECRET_KEY")
+        self.RECAPTCHA_PUBLIC_KEY = environ.get("RECAPTCHA_PUBLIC_KEY")
+        self.RECAPTCHA_PRIVATE_KEY = environ.get("RECAPTCHA_PRIVATE_KEY")
+        self.UPLOADS_BASE_PATH = environ.get("UPLOADS_BASE_PATH")
+        self.DB_PATH = environ.get('DB_PATH')
 
 class DevelopmentConfig(Config):
-    if not Config.SECRET_KEY:
-        raise Exception("SECRET_KEY must be provided in .env file.")
-    if not Config.RECAPTCHA_PUBLIC_KEY:
-        raise Exception("RECAPTCHA_PUBLIC_KEY must be provided in .env file.")
-    if not Config.RECAPTCHA_PRIVATE_KEY:
-        raise Exception("RECAPTCHA_PRIVATE_KEY must be provided in .env file.")
+    def __init__(self):
+        super().__init__()
+        if not self.SECRET_KEY:
+            raise Exception("SECRET_KEY must be provided in .env file.")
+        if not self.RECAPTCHA_PUBLIC_KEY:
+            raise Exception("RECAPTCHA_PUBLIC_KEY must be provided in .env file.")
+        if not self.RECAPTCHA_PRIVATE_KEY:
+            raise Exception("RECAPTCHA_PRIVATE_KEY must be provided in .env file.")
 
 class TestConfig(Config):
-    if not Config.SECRET_KEY:
-        SECRET_KEY = token_hex()
-    if not Config.RECAPTCHA_PUBLIC_KEY:
-        RECAPTCHA_PUBLIC_KEY = ''
-    if not Config.RECAPTCHA_PRIVATE_KEY:
-        RECAPTCHA_PRIVATE_KEY = ''
-    TESTING = True
-    WTF_CSRF_ENABLED = False
-
     def __init__(self, db_path, uploads_base_path):
-        TestConfig.DB_PATH = db_path
-        TestConfig.UPLOADS_BASE_PATH = uploads_base_path
+        super().__init__()
+
+        self.DB_PATH = db_path
+        self.UPLOADS_BASE_PATH = uploads_base_path
+        if not self.SECRET_KEY:
+            self.SECRET_KEY = token_hex()
+        if not self.RECAPTCHA_PUBLIC_KEY:
+            self.RECAPTCHA_PUBLIC_KEY = ''
+        if not self.RECAPTCHA_PRIVATE_KEY:
+            self.RECAPTCHA_PRIVATE_KEY = ''
+        self.TESTING = True
+        self.WTF_CSRF_ENABLED = False
