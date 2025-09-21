7602713f5a7e85a8e97c16850303ad43b86b0bbb
Ayman Zaher <11262003a@gmail.com>
2025-09-21 13:29:08 -0400

    Implemented Form Validation Feedback and Password Visibility

--- website/forms/login_form.py
+++ website/forms/login_form.py
@@ -3,7 +3,7 @@ from wtforms import StringField, SubmitField, PasswordField, BooleanField
 from wtforms.validators import DataRequired
 
 class LoginForm(FlaskForm):
-    username_or_email = StringField('Email/Username', validators=[DataRequired()], render_kw={"placeholder": "Enter your email or username"})
+    username_or_email = StringField('Email or Username', validators=[DataRequired()], render_kw={"placeholder": "Enter your email or username"})
     password = PasswordField('Password', validators=[DataRequired()], render_kw={"placeholder": "Enter your password"})
     remember_me = BooleanField('Remember Me')
     submit = SubmitField('Login')
---
+++ website/static/js/form-script.js
@@ -0,0 +1,60 @@
+let eyeSpriteUrl = null;
+
+document.addEventListener("DOMContentLoaded", () => {
+    const forms = document.querySelectorAll('form.needs-validation');
+
+    // Loop over them and prevent submission
+    Array.from(forms).forEach(form => {
+        form.addEventListener('submit', event => {
+        if (!form.checkValidity()) {
+            event.preventDefault();
+            event.stopPropagation();
+        }
+
+        form.classList.add('was-validated');
+        }, false)
+    })
+
+    eyeSpriteUrl = document.querySelector('meta[name=eye-sprite-url]').content;
+
+    const visibilityToggles = document.querySelectorAll('button.password-visibility-toggle');
+    Array.from(visibilityToggles).forEach(button => {
+        button.addEventListener("click", togglePasswordVisibility);
+    })
+
+    const passwordInput = document.getElementById("password");
+    if (passwordInput === null || passwordInput.tagName !== "INPUT") {
+        return;
+    }
+    const confirmPasswordInput = document.getElementById("confirm_password");
+    if (confirmPasswordInput === null || confirmPasswordInput.tagName !== "INPUT") {
+        return;
+    }
+
+    const validatePasswordMatch = () => {
+        if (confirmPasswordInput.value !== passwordInput.value) {
+            confirmPasswordInput.setCustomValidity("Passwords do not match");
+        } else {
+            confirmPasswordInput.setCustomValidity("");
+        }
+    };
+
+    passwordInput.addEventListener('input', validatePasswordMatch);
+    confirmPasswordInput.addEventListener('input', validatePasswordMatch);
+});
+
+function togglePasswordVisibility() {
+    const inputField = this.parentElement.querySelector("input");
+    if (inputField === null) {
+        throw new Error("The visibility toggle is not utilizing the expected layout.");
+    }
+
+    if (inputField.getAttribute("type") === "password") {
+        inputField.setAttribute("type", "text");
+        this.querySelector("use").setAttribute("href", `${eyeSpriteUrl}#icon-eye-slash`);
+        return;
+    }
+
+    inputField.setAttribute("type", "password");
+    this.querySelector("use").setAttribute("href", `${eyeSpriteUrl}#icon-eye`);
+}
