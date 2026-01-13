1a5ed2c4c84164fc0269ee208d011368c679be5a
Ayman Zaher <11262003a@gmail.com>
2026-01-12 19:42:02 -0500

    Fixed test case failures after tag system overhaul
    - Also upgraded all packages

--- tests/test_meme.py
+++ tests/test_meme.py
@@ -1,52 +1,54 @@
 from io import BytesIO
 from os import path
+
 from werkzeug.datastructures import FileStorage
 
 from .base_test_class import BaseTestClass
+
 
 class MemeTests(BaseTestClass):
     def test_get_meme_image(self):
         """Test retrieving the image for a meme"""
         user_id = self.createTestUser()
-        meme_id = self.createTestMeme(user_id, 'test meme')
-        response = self.client.get(f'/meme/image/{meme_id}')
+        meme_id = self.createTestMeme(user_id, "test meme")
+        response = self.client.get(f"/meme/image/{meme_id}")
         self.assertEqual(response.status_code, 200)
-        test_meme_image = path.join(self.test_dir, 'test_meme.jpg')
-        with open(test_meme_image, 'rb') as meme_image:
+        test_meme_image = path.join(self.test_dir, "test_meme.jpg")
+        with open(test_meme_image, "rb") as meme_image:
             self.assertEqual(response.data, meme_image.read())
 
     def test_get_nonexisent_meme_image(self):
         """Test retrieving the image for a nonexistent meme"""
-        response = self.client.get('/meme/image/nonexistent')
+        response = self.client.get("/meme/image/nonexistent")
         self.assertEqual(response.status_code, 404)
 
     def test_view_meme(self):
         """Test viewing a meme page"""
         user_id = self.createTestUser()
-        meme_id = self.createTestMeme(user_id, 'test meme')
-        response = self.client.get(f'/meme/{meme_id}')
+        meme_id = self.createTestMeme(user_id, "test meme")
+        response = self.client.get(f"/meme/{meme_id}")
         self.assertEqual(response.status_code, 200)
-        self.assertIn(f'/meme/image/{meme_id}', response.text)
+        self.assertIn(f"/meme/image/{meme_id}", response.text)
 
     def test_view_nonexistent_meme(self):
-        response = self.client.get('/meme/1')
+        response = self.client.get("/meme/1")
         self.assertEqual(response.status_code, 404)
 
     def test_meme_search(self):
         """Test searching for memes by tags"""
         user_id = self.createTestUser()
-        meme_id = self.createTestMeme(user_id, 'testtag')
-        response = self.client.get('/search?search=testtag')
+        meme_id = self.createTestMeme(user_id, "testtag")
+        response = self.client.get("/search?search=testtag")
         self.assertEqual(response.status_code, 200)
-        self.assertIn('testtag', response.text)
+        self.assertIn("testtag", response.text)
         self.assertIn(f'data-meme-id="{meme_id}"', response.text)
 
     def test_meme_search_lazy_load(self):
         """Test search meme lazy loading"""
         user_id = self.createTestUser()
-        expected_id = self.createTestMeme(user_id, 'testtag')
-        last_id = self.createTestMeme(user_id, 'testtag')
-        response = self.client.get(f'/search?search=testtag&last_id={last_id}')
+        expected_id = self.createTestMeme(user_id, "testtag")
+        last_id = self.createTestMeme(user_id, "testtag")
+        response = self.client.get(f"/search?search=testtag&last_id={last_id}")
         self.assertEqual(response.status_code, 200)
         self.assertIn(f'data-meme-id="{expected_id}"', response.text)
         self.assertNotIn(f'data-meme-id="{last_id}"', response.text)
@@ -54,191 +56,208 @@ class MemeTests(BaseTestClass):
     def test_no_result_meme_search(self):
         """Test searching for memes by a nonexistent tag"""
         user_id = self.createTestUser()
-        meme_id = self.createTestMeme(user_id, 'testtag')
-        response = self.client.get('/search?search=nonexistent')
+        meme_id = self.createTestMeme(user_id, "testtag")
+        response = self.client.get("/search?search=nonexistent")
         self.assertEqual(response.status_code, 200)
-        self.assertIn('nonexistent', response.text)
-        self.assertNotIn('testtag', response.text)
-        self.assertNotIn(f'meme_{meme_id}', response.text)
+        self.assertIn("nonexistent", response.text)
+        self.assertIn(
+            '<div id="memes" class="meme-container">\n    \n</div>', response.text
+        )
+        self.assertNotIn(f"meme_{meme_id}", response.text)
 
     def test_invalid_search(self):
         """Test search without a query"""
-        response = self.client.get('/search')
+        response = self.client.get("/search")
         self.assertEqual(response.status_code, 302)
-        self.assertEqual(response.location, '/')
+        self.assertEqual(response.location, "/")
 
     def test_like_meme(self):
         """Test liking a meme"""
         user_id = self.createTestUser()
-        meme_id = self.createTestMeme(user_id, 'test meme')
+        meme_id = self.createTestMeme(user_id, "test meme")
         with self.logged_in_context(user_id):
-            response = self.client.post('/api/toggleLike', data=dict(
-                id=meme_id
-            ))
+            response = self.client.post("/api/toggleLike", data=dict(id=meme_id))
             self.assertEqual(response.status_code, 200)
             self.assertIn("#unlike-icon", response.text)
 
     def test_unlike_meme(self):
         """Test unliking a meme"""
         user_id = self.createTestUser()
-        meme_id = self.createTestMeme(user_id, 'test meme')
+        meme_id = self.createTestMeme(user_id, "test meme")
         self.likeMeme(meme_id, user_id)
         with self.logged_in_context(user_id):
-            response = self.client.post('/api/toggleLike', data=dict(
-                id=meme_id
-            ))
+            response = self.client.post("/api/toggleLike", data=dict(id=meme_id))
             self.assertEqual(response.status_code, 200)
             self.assertIn("#like-icon", response.text)
 
     def test_toggleLike_logged_out(self):
         user_id = self.createTestUser()
         meme_id = self.createTestMeme(user_id, "test meme")
-        response = self.client.post('/api/toggleLike', data=dict(
-            id=meme_id
-        ))
+        response = self.client.post("/api/toggleLike", data=dict(id=meme_id))
         self.assertEqual(response.status_code, 401)
 
     def test_toggleLike_invalid_request(self):
         user_id = self.createTestUser()
         with self.logged_in_context(user_id):
-            response = self.client.post('/api/toggleLike', data=dict())
+            response = self.client.post("/api/toggleLike", data=dict())
             self.assertEqual(response.status_code, 400)
 
     def test_toggleLike_nonexistent_meme(self):
         user_id = self.createTestUser()
         with self.logged_in_context(user_id):
-            response = self.client.post('/api/toggleLike', data=dict(
-                id=1
-            ))
+            response = self.client.post("/api/toggleLike", data=dict(id=1))
             self.assertEqual(response.status_code, 404)
 
     def test_upload_meme(self):
         user_id = self.createTestUser()
-        meme_image = open(path.join(self.test_dir, 'test_meme.jpg'), 'rb')
+        meme_image = open(path.join(self.test_dir, "test_meme.jpg"), "rb")
         meme_file = FileStorage(
             stream=BytesIO(meme_image.read()),
-            content_type='image/jpeg',
-            filename='randomname.jpg'
+            content_type="image/jpeg",
+            filename="randomname.jpg",
         )
         meme_image.close()
-        
+
         with self.logged_in_context(user_id):
-            response = self.client.post('/upload', data= dict(
-                meme=meme_file,
-                tags='[{"value": "test meme"}]'
-            ), content_type='multipart/form-data')
+            response = self.client.post(
+                "/upload",
+                data=dict(meme=meme_file, tags='[{"value": "test meme"}]'),
+                content_type="multipart/form-data",
+            )
             self.assertEqual(response.status_code, 302)
-            self.assertEqual(response.location, '/')
+            self.assertEqual(response.location, "/")
 
     def test_upload_meme_existent_tag(self):
         user_id = self.createTestUser()
         self.createTag("test meme")
-        meme_image = open(path.join(self.test_dir, 'test_meme.jpg'), 'rb')
+        meme_image = open(path.join(self.test_dir, "test_meme.jpg"), "rb")
         meme_file = FileStorage(
             stream=BytesIO(meme_image.read()),
-            content_type='image/jpeg',
-            filename='randomname.jpg'
+            content_type="image/jpeg",
+            filename="randomname.jpg",
         )
         meme_image.close()
-        
+
         with self.logged_in_context(user_id):
-            response = self.client.post('/upload', data= dict(
-                meme=meme_file,
-                tags='[{"value": "test meme"}]'
-            ), content_type='multipart/form-data')
+            response = self.client.post(
+                "/upload",
+                data=dict(meme=meme_file, tags='[{"value": "test meme"}]'),
+                content_type="multipart/form-data",
+            )
             self.assertEqual(response.status_code, 302)
-            self.assertEqual(response.location, '/')
+            self.assertEqual(response.location, "/")
 
     def test_upload_meme_missing_image(self):
         user_id = self.createTestUser()
         with self.logged_in_context(user_id):
-            response = self.client.post('/upload', data= dict(
-                tags='[{"value": "test meme"}]'
-            ), content_type='multipart/form-data')
+            response = self.client.post(
+                "/upload",
+                data=dict(tags='[{"value": "test meme"}]'),
+                content_type="multipart/form-data",
+            )
             self.assertEqual(response.status_code, 200)
-            self.assertIn("Error in the Choose image field - This field is required.", response.text)
+            self.assertIn(
+                "Error in the Choose image field - This field is required.",
+                response.text,
+            )
 
     def test_upload_meme_missing_tag(self):
         user_id = self.createTestUser()
-        meme_image = open(path.join(self.test_dir, 'test_meme.jpg'), 'rb')
+        meme_image = open(path.join(self.test_dir, "test_meme.jpg"), "rb")
         meme_file = FileStorage(
             stream=BytesIO(meme_image.read()),
-            content_type='image/jpeg',
-            filename='randomname.jpg'
+            content_type="image/jpeg",
+            filename="randomname.jpg",
         )
         meme_image.close()
-        
+
         with self.logged_in_context(user_id):
-            response = self.client.post('/upload', data= dict(
-                meme=meme_file,
-            ), content_type='multipart/form-data')
+            response = self.client.post(
+                "/upload",
+                data=dict(
+                    meme=meme_file,
+                ),
+                content_type="multipart/form-data",
+            )
             self.assertEqual(response.status_code, 200)
-            self.assertIn("Error in the Tags field - This field is required.", response.text)
+            self.assertIn(
+                "Error in the Tags field - This field is required.", response.text
+            )
 
     def test_upload_meme_non_image(self):
         user_id = self.createTestUser()
         meme_file = FileStorage(
-            stream=BytesIO(),
-            content_type='image/jpeg',
-            filename='randomname.jpg'
+            stream=BytesIO(), content_type="image/jpeg", filename="randomname.jpg"
         )
 
         with self.logged_in_context(user_id):
-            response = self.client.post('/upload', data= dict(
-                meme=meme_file,
-                tags='[{"value": "test meme"}]'
-            ), content_type='multipart/form-data')
+            response = self.client.post(
+                "/upload",
+                data=dict(meme=meme_file, tags='[{"value": "test meme"}]'),
+                content_type="multipart/form-data",
+            )
             self.assertEqual(response.status_code, 200)
-            self.assertIn("The uploaded meme is not in a supported format. Please upload a proper image.", response.text)
+            self.assertIn(
+                "The uploaded meme is not in a supported format. Please upload a proper image.",
+                response.text,
+            )
 
     def test_delete_meme(self):
         user_id = self.createTestUser()
-        meme_id = self.createTestMeme(user_id, 'test meme')
+        meme_id = self.createTestMeme(user_id, "test meme")
         with self.logged_in_context(user_id):
-            response = self.client.post("/delete", data=dict(
-                id=meme_id
-            ), content_type='multipart/form-data')
+            response = self.client.post(
+                "/delete", data=dict(id=meme_id), content_type="multipart/form-data"
+            )
             self.assertEqual(response.status_code, 302)
-            self.assertEqual(response.location, '/')
-    
+            self.assertEqual(response.location, "/")
+
     def test_delete_meme_logged_out(self):
         user_id = self.createTestUser()
         meme_id = self.createTestMeme(user_id, "test meme")
-        response = self.client.post("/delete", data=dict(
-            id=meme_id
-        ), content_type='multipart/form-data')
+        response = self.client.post(
+            "/delete", data=dict(id=meme_id), content_type="multipart/form-data"
+        )
         self.assertEqual(response.status_code, 302)
         self.assertStartsWith(response.location, "/login")
-    
+
     def test_delete_meme_invalid_request(self):
         user_id = self.createTestUser()
-        self.createTestMeme(user_id, 'test meme')
+        self.createTestMeme(user_id, "test meme")
         with self.logged_in_context(user_id):
-            response = self.client.post("/delete", data=dict(), content_type='multipart/form-data')
+            response = self.client.post(
+                "/delete", data=dict(), content_type="multipart/form-data"
+            )
             self.assertEqual(response.status_code, 302)
             with self.client.session_transaction() as session:
                 messages = session["_flashes"]
                 self.assertGreater(len(messages), 0)
-                self.assertIn("There was an error while processing your request, please try again.", [x[1] for x in messages])
+                self.assertIn(
+                    "There was an error while processing your request, please try again.",
+                    [x[1] for x in messages],
+                )
 
     def test_delete_nonexistent_meme(self):
         user_id = self.createTestUser()
         with self.logged_in_context(user_id):
-            response = self.client.post("/delete", data=dict(
-                id=1
-            ), content_type='multipart/form-data')
+            response = self.client.post(
+                "/delete", data=dict(id=1), content_type="multipart/form-data"
+            )
             self.assertEqual(response.status_code, 302)
             with self.client.session_transaction() as session:
                 messages = session["_flashes"]
                 self.assertGreater(len(messages), 0)
-                self.assertIn("Unable to locate meme, it may have been deleted.", [x[1] for x in messages])
+                self.assertIn(
+                    "Unable to locate meme, it may have been deleted.",
+                    [x[1] for x in messages],
+                )
 
     def test_delete_other_user_meme(self):
         first_user_id = self.createTestUser()
         second_user_id = self.createTestUser()
         meme_id = self.createTestMeme(first_user_id, "test meme")
         with self.logged_in_context(second_user_id):
-            response = self.client.post("/delete", data=dict(
-                id=meme_id
-            ), content_type='multipart/form-data')
+            response = self.client.post(
+                "/delete", data=dict(id=meme_id), content_type="multipart/form-data"
+            )
             self.assertEqual(response.status_code, 401)
--- tests_e2e/test_meme.py
+++ tests_e2e/test_meme.py
@@ -1,12 +1,13 @@
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.common.exceptions import NoSuchElementException
-
 import os
 import time
 
+from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+
 from .base_test_class import BaseTestClass
+
 
 class MemeTests(BaseTestClass):
     def test_upload_meme(self):
@@ -20,24 +21,26 @@ class MemeTests(BaseTestClass):
             )
 
             file_select = upload_form.find_element(By.CSS_SELECTOR, "input[type=file]")
-            file_select.send_keys(os.path.join(self.test_dir, 'test_meme.jpg'))
+            file_select.send_keys(os.path.join(self.test_dir, "test_meme.jpg"))
 
             tag_input = upload_form.find_element(By.CSS_SELECTOR, "tags span")
             tag_input.send_keys("test tag")
 
-            upload_button = upload_form.find_element(By.CSS_SELECTOR, "input[type=submit]")
+            upload_button = upload_form.find_element(
+                By.CSS_SELECTOR, "input[type=submit]"
+            )
             upload_button.click()
 
-            wait = WebDriverWait(self.driver, 10).until(
-                EC.url_changes(upload_path)
-            )
+            wait = WebDriverWait(self.driver, 10).until(EC.url_changes(upload_path))
 
             self.assertTrue(wait)
 
             self.assertTrue(self.pageFlashesContain("Meme Successfully Uploaded!"))
 
-            tag_link = self.driver.find_element(By.XPATH, '//*[@id="memes"]/div[1]/div/div/a')
-            self.assertEqual(tag_link.text, "test tag")
+            tag_link = self.driver.find_element(
+                By.XPATH, '//*[@id="memes"]/div[1]/div/div/div/a'
+            )
+            self.assertEqual(tag_link.text, "#test tag")
 
     def test_view_meme(self):
         user_id = self.createTestUser()
@@ -47,23 +50,25 @@ class MemeTests(BaseTestClass):
         self.driver.get(home_path)
 
         meme_details_link = WebDriverWait(self.driver, 10).until(
-            EC.presence_of_element_located((By.CSS_SELECTOR, f".meme a[href='/meme/{meme_id}']"))
+            EC.presence_of_element_located(
+                (By.CSS_SELECTOR, f".meme a[href='/meme/{meme_id}']")
+            )
         )
         meme_details_link.click()
 
-        wait = WebDriverWait(self.driver, 10).until(
-            EC.url_changes(home_path)
-        )
+        wait = WebDriverWait(self.driver, 10).until(EC.url_changes(home_path))
 
         self.assertTrue(wait)
 
         full_meme_path = self.getFullWebPath(f"/meme/{meme_id}")
         self.assertEqual(self.driver.current_url, full_meme_path)
 
-        image_element = self.driver.find_element(By.CSS_SELECTOR, f"img[src='/meme/image/{meme_id}']")
+        image_element = self.driver.find_element(
+            By.CSS_SELECTOR, f"img[src='/meme/image/{meme_id}']"
+        )
         is_image_loaded = self.driver.execute_script(
             "return arguments[0].complete && typeof arguments[0].naturalWidth != 'undefined' && arguments[0].naturalWidth > 0",
-            image_element
+            image_element,
         )
         self.assertTrue(is_image_loaded)
 
@@ -76,7 +81,9 @@ class MemeTests(BaseTestClass):
             self.driver.get(full_meme_path)
 
             delete_trigger = WebDriverWait(self.driver, 10).until(
-                EC.presence_of_element_located((By.CSS_SELECTOR, "button.link-danger[data-bs-toggle='modal']"))
+                EC.presence_of_element_located(
+                    (By.CSS_SELECTOR, "button.link-danger[data-bs-toggle='modal']")
+                )
             )
 
             delete_trigger.click()
@@ -85,20 +92,25 @@ class MemeTests(BaseTestClass):
                 EC.visibility_of_element_located((By.ID, "deleteModal"))
             )
 
-            delete_button = delete_modal.find_element(By.CSS_SELECTOR, "button[type=submit]")
+            delete_button = delete_modal.find_element(
+                By.CSS_SELECTOR, "button[type=submit]"
+            )
             delete_button.click()
 
-            wait = WebDriverWait(self.driver, 10).until(
-                EC.url_changes(full_meme_path)
-            )
+            wait = WebDriverWait(self.driver, 10).until(EC.url_changes(full_meme_path))
 
             self.assertTrue(wait)
 
             home_path = self.getFullWebPath("/")
             self.assertEqual(self.driver.current_url, home_path)
-            
+
             self.assertTrue(self.pageFlashesContain("Successfully deleted meme!"))
-            self.assertRaises(NoSuchElementException, self.driver.find_element, By.CSS_SELECTOR, f".meme a[href='/meme/{meme_id}']")
+            self.assertRaises(
+                NoSuchElementException,
+                self.driver.find_element,
+                By.CSS_SELECTOR,
+                f".meme a[href='/meme/{meme_id}']",
+            )
 
     def test_download_meme(self):
         user_id = self.createTestUser()
@@ -110,7 +122,9 @@ class MemeTests(BaseTestClass):
         meme_filename = f"meme_{meme_id}.jpg"
 
         download_button = WebDriverWait(self.driver, 10).until(
-            EC.presence_of_element_located((By.CSS_SELECTOR, f"a[download='{meme_filename}']"))
+            EC.presence_of_element_located(
+                (By.CSS_SELECTOR, f"a[download='{meme_filename}']")
+            )
         )
         download_button.click()
 
